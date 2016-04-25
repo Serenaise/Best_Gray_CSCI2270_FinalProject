8e117b2cc73f03b5f1115d8c605aad0274cfc6ff
Serenaise <kabe3951@colorado.edu>
2016-04-25 11:40:39 -0600

    Updated .cpp

--- HashTable.cpp
+++ HashTable.cpp
@@ -201,124 +201,126 @@ int HashTable::hashMultiply(string k, int sz)
   intpart = floor(intpart);
   return intpart;
 }
-/*This function does not work*/
-/*void HashTable::SHA1(string message, uint32_t hh[5])
 
-SHA1("")
-gives hexadecimal: da39a3ee5e6b4b0d3255bfef95601890afd80709
-
+/*This DOES NOT break.  Don't do anything to it!*/
+void HashTable::SHA1(string message, uint32_t hh[5])
 {
-  union {
-    uint64_t Int64;
-    char Chars[sizeof(uint64_t)];
-  } var;
-  uint32_t h0 = 0x67452301;
-  uint32_t h1 = 0xEFCDAB89;
-  uint32_t h2 = 0x98BADCFE;
-  uint32_t h3 = 0x10325476;
-  uint32_t h4 = 0xC3D2E1F0;
-  uint64_t ml = (message.length())*8;*//*The message length in bits*/
-  /*message.push_back(0x80);
-  while ((message.length()*8)%512 != 448)
+  /*SHA1("")
+  gives hexadecimal: da39a3ee5e6b4b0d3255bfef95601890afd80709*/
   {
-    cout << "Before: " << message.length() << endl;
-    message.push_back(0x00);
-    cout << "After: " << message.length() << endl;
-  }
-  ml = htobe64(ml);
-  var.Int64 = ml;
-  for(int i = 0; i < sizeof(uint64_t); ++i)
-  {
-    message.push_back(var.Chars[i]);
-  }
-  vector <vector <uint8_t> > ChunkV;
-  size_t ChunkOffset;
-  for (int x = 0; x < message.length()/64; x++)
-  {
-    vector <uint8_t> chunk;
-    ChunkOffset = x * 64;
-    for (int byteI = 0; byteI < 64; ++byteI)
+    union {
+      uint64_t Int64;
+      char Chars[sizeof(uint64_t)];
+    } var;
+    uint32_t h0 = 0x67452301;
+    uint32_t h1 = 0xEFCDAB89;
+    uint32_t h2 = 0x98BADCFE;
+    uint32_t h3 = 0x10325476;
+    uint32_t h4 = 0xC3D2E1F0;
+    uint64_t ml = (message.length())*8;/*The message length in bits*/
+    message.push_back(0x80);
+    while ((message.length()*8)%512 != 448)
     {
-      chunk.push_back(message[ChunkOffset + byteI]);
-      //cout << "Pushing: " << (unsigned int)message[ChunkOffset + byteI] <<endl;
+      //cout << "Before: " << message.length() << endl;
+      message.push_back(0x00);
+      //cout << "After: " << message.length() << endl;
     }
-    ChunkV.push_back(chunk);
-  }
-  //uint32_t w[16];
-  for (int CI = 0; CI < ChunkV.size(); CI ++)
-  {
-    for (int ci = 0; ci < ChunkV[CI].size(); ci++)
+    ml = htobe64(ml);
+    var.Int64 = ml;
+    for(int i = 0; i < sizeof(uint64_t); ++i)
     {
-      cout << (int)ChunkV[CI][ci] << endl;
+      message.push_back(var.Chars[i]);
     }
-  }
-  for(int x = 0; x < ChunkV.size(); ++x)
-  {
-    uint32_t w[80];*/
-    /*uint32_t *wordptr;
-    wordptr = (uint32_t*)&ChunkV[x][0];
-    w[i] = *wordptr;*/
-    /*for(int i = 0,j = 0; i < 64; i += 4, j++)
+    vector <vector <uint8_t> > ChunkV;
+    size_t ChunkOffset;
+    for (int x = 0; x < message.length()/64; x++)
     {
-      w[j] = 0;
-      w[j] = ((uint32_t)ChunkV[x][i]) << 24 | ((uint32_t)ChunkV[x][i + 1]) << 16 | ((uint32_t)ChunkV[x][i + 2]) << 8 | ((uint32_t)ChunkV[x][i + 3]);
-      w[j] = htobe32(w[j]);
+      vector <uint8_t> chunk;
+      ChunkOffset = x * 64;
+      for (int byteI = 0; byteI < 64; ++byteI)
+      {
+        chunk.push_back(message[ChunkOffset + byteI]);
+        //cout << "Pushing: " << (unsigned int)message[ChunkOffset + byteI] <<endl;
+      }
+      ChunkV.push_back(chunk);
     }
-    for (int i = 16; i < 80; i++)
+    //uint32_t w[16];
+    for (int CI = 0; CI < ChunkV.size(); CI ++)
     {
-      w[i] = leftRotate((w[i-3] ^ w[i-8] ^ w[i-14] ^ w[i-16]),1);
+      for (int ci = 0; ci < ChunkV[CI].size(); ci++)
+      {
+        //cout << (int)ChunkV[CI][ci] << endl;
+      }
     }
-    uint32_t a = h0;
-    uint32_t b = h1;
-    uint32_t c = h2;
-    uint32_t d = h3;
-    uint32_t e = h4;
-    uint32_t f;
-    uint32_t k;
-    uint32_t temp;
-    for (int i = 0; i < 80; ++i)
+    for(int x = 0; x < ChunkV.size(); ++x)
     {
-      if (0 <= i <= 19)
+      uint32_t w[80];
+      /*uint32_t *wordptr;
+      wordptr = (uint32_t*)&ChunkV[x][0];
+      w[i] = *wordptr;*/
+      for(int i = 0,j = 0; i < 64; i += 4, j++)
       {
-        f = (b & c) | ((~b) & d);
-        k = 0x5A827999;
+        w[j] = 0;
+        w[j] = ((uint32_t)ChunkV[x][i]) << 24 | ((uint32_t)ChunkV[x][i + 1]) << 16 | ((uint32_t)ChunkV[x][i + 2]) << 8 | ((uint32_t)ChunkV[x][i + 3]);
+        w[j] = htobe32(w[j]);
       }
-      else if (20 <= i <= 39)
+      for (int i = 16; i < 80; i++)
       {
-        f = b ^ c ^ d;
-        k = 0x6ED9EBA1;
+        w[i] = leftRotate((w[i-3] ^ w[i-8] ^ w[i-14] ^ w[i-16]),1);
       }
-      else if (40 <= i <= 59)
+      uint32_t a = h0;
+      uint32_t b = h1;
+      uint32_t c = h2;
+      uint32_t d = h3;
+      uint32_t e = h4;
+      uint32_t f;
+      uint32_t k;
+      uint32_t temp;
+      for (int i = 0; i < 80; ++i)
       {
-        f = (b & c) | (b & d) | (c & d);
-        k = 0x8F1BBCDC;
-      }
-      else if (60 <= i <= 79)
-      {
-        f = b ^ c ^ d;
-        k = 0xCA62C1D6;
-      }
+        if (0 <= i <= 19)
+        {
+          f = (b & c) | ((~b) & d);
+          k = 0x5A827999;
+        }
+        else if (20 <= i <= 39)
+        {
+          f = b ^ c ^ d;
+          k = 0x6ED9EBA1;
+        }
+        else if (40 <= i <= 59)
+        {
+          f = (b & c) | (b & d) | (c & d);
+          k = 0x8F1BBCDC;
+        }
+        else if (60 <= i <= 79)
+        {
+          f = b ^ c ^ d;
+          k = 0xCA62C1D6;
+        }
 
-    temp = (leftRotate(a,5) + f + e + k + w[i]);
-    e = d;
-    d = c;
-    c = leftRotate(b,30);
-    b = a;
-    a = temp;
-    h0 = h0 + a;
-    h1 = h1 + b;
-    h2 = h2 + c;
-    h3 = h3 + d;
-    h4 = h4 + e;
+      temp = (leftRotate(a,5) + f + e + k + w[i]);
+      e = d;
+      d = c;
+      c = leftRotate(b,30);
+      b = a;
+      a = temp;
+      h0 = h0 + a;
+      h1 = h1 + b;
+      h2 = h2 + c;
+      h3 = h3 + d;
+      h4 = h4 + e;
+      }
     }
+    hh[5];
+    hh[0] = h0;
+    hh[1] = h1;
+    hh[2] = h2;
+    hh[3] = h3;
+    hh[4] = h4;
   }
-  hh[5];
-  hh[0] = h0;
-  hh[1] = h1;
-  hh[2] = h2;
-  hh[3] = h3;
-  hh[4] = h4;
 }
+/*Also works fine*/
 uint32_t HashTable::leftRotate(uint32_t val, int itt)
 {
   val = be32toh(val);
@@ -339,8 +341,7 @@ uint32_t HashTable::leftRotate(uint32_t val, int itt)
     return val;
   }
 
-}*/
-
+}
 
 int HashTable::hashScrabble(std::string str, int sz){
   int sum = 0;
